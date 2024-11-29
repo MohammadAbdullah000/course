bd94a3ef544ae99adbbd68b88d7588ec7150da13
Mohammad Abdullah <abdullah@rioforge.com>
2024-11-29 17:43:29 +0530

    addmission and swiper

--- script.js
+++ script.js
@@ -26,25 +26,3 @@ document.getElementById('scrollRight').addEventListener('click', function() {
     container.scrollBy({ left: 300, behavior: 'smooth' });
 });
 
-// Get elements
-const contactButton = document.getElementById('contactButton');
-const contactPopup = document.getElementById('contactPopup');
-const closePopup = document.getElementById('closePopup');
-
-// Open the popup
-contactButton.addEventListener('click', (e) => {
-  e.preventDefault(); // Prevent default link behavior
-  contactPopup.style.display = 'flex';
-});
-
-// Close the popup
-closePopup.addEventListener('click', () => {
-  contactPopup.style.display = 'none';
-});
-
-// Close the popup when clicking outside the content
-window.addEventListener('click', (e) => {
-  if (e.target === contactPopup) {
-    contactPopup.style.display = 'none';
-  }
-});
